1ed96d9870c874ad7eadf69fd3150f2ffa797c86
sehcheong-ng <ngsehcheong@hotmail.com>
2022-10-21 22:41:53 +0100

    add JS script

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,27 @@
-var topic = "CSS";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if (topic === 'HTML'){
-    console.log("Let's study HTML!");
-
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
-} 
-
-
-var shapes = ["triangle", "square", "pentagon", "circle"];
-
-console.log(shapes);
-console.log(shapes[2]);
-    
-for (i in shapes) {
-    console.log(shapes[i]);
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
 
-function listShapes(){
-    for (var x = 0; x < shapes.length; x++) {
-        console.log(shapes[x])
-    }
+function selectTopic() {
+ if (randomTopic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (randomTopic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (randomTopic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
 
-listShapes();
-
-
-var randomShapes = shapes[Math.floor(Math.random() * shapes.length)];
-
-console.log(Math.floor(5.6));
-console.log(Math.floor(5.4));
-
-console.log(Math.random());
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
