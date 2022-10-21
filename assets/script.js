91ba86270386c51007efa4f4e779e0d832a9d4ef
sehcheong-ng <ngsehcheong@hotmail.com>
2022-10-21 21:25:10 +0100

    add Javascript

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topic = "CSS";
+
+if (topic === 'HTML'){
+    console.log("Let's study HTML!");
+
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+} 
+
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+
+console.log(shapes);
+console.log(shapes[2]);
+    
+for (i in shapes) {
+    console.log(shapes[i]);
+}
